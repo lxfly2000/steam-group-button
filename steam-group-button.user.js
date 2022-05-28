771b273ed5378910338a3e8c4691c8ec44bd6e12
Leaf <gaoboyuan8@qq.com>
2022-05-28 20:02:20 +0800

    Update steam-group-button.user.js

--- steam-group-button.user.js
+++ steam-group-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam官方组按钮
 // @namespace    https://github.com/lxfly2000/steam-group-button
-// @version      1.1.1
+// @version      1.1.2
 // @updateURL    https://github.com/lxfly2000/steam-group-button/raw/master/steam-group-button.user.js
 // @downloadURL  https://github.com/lxfly2000/steam-group-button/raw/master/steam-group-button.user.js
 // @description  在Steam商店页和社区页添加官方组按钮
@@ -19,8 +19,8 @@ function SteamGroupButton_AddButton(){
         if(itbtn.href!==undefined&&rapp.test(itbtn.href)){
             var appid=itbtn.href.match(rapp)[2];
             var newNode=itbtn.cloneNode(true);
-            newNode.href=location.protocol+"//steamcommunity.com/games/"+appid;
-            newNode.innerHTML="<span>官方组</span>";
+            newNode.href=location.protocol+"//steamcommunity.com/games/"+appid+"/Avatar/List";
+            newNode.innerHTML="<span>头像</span>";
             itbtn.parentElement.insertBefore(newNode,itbtn);
             itbtn.parentElement.insertBefore(document.createTextNode(" "),itbtn);
             break;
